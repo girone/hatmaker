85028dbb0dbb4786b5b1c272e03d126e2d5b5c9b
Jonas Sternisko <jonas.sternisko@gmail.com>
2015-08-24 22:09:25 +0200

    Add a set of sed commands to fetch directly from mysql.
    Don't bother taking the workaround by exporting the CSV by hand,
    anymore.

--- get_data.sh
+++ get_data.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
+
 # This script is used to fetch all player information and output it as CSV.
 # The output can be stored in a file and then processed with the python script
 # format_data.py.
 
+# Read the credentials from file:
+if [ -e "credentials.txt" ]
+then
+    server=$(head -1 credentials.txt)
+    user=$(head -2 credentials.txt | tail -1); 
+    pass=$(head -3 credentials.txt | tail -1);
+    db=$(head -4 credentials.txt | tail -1);
+else
+    echo "Error: File credentials.txt not found."
+    exit 1
+fi
 
-# NOTE: It seemed more convenient to just export the data as csv from
-# phpMyAdmin.
+# enclose with double-quotes 
+# remove end-of-line characters 
+# remove windows end-of-line characters 
+# replace comma by # (undo later)
+# replace tabs separators by ","
+# skip the column header line
+mysql -h $server -u$user -p$pass $db \
+  --batch --execute "SELECT * FROM MischMasch;" | \
+sed 's/^\(.*\)$/"\1"/g' | \
+sed $'s/\r//g' | \
+sed 's/\\n//g' | \
+sed 's/,/\\#/g' | \
+sed 's/\t/","/g' | \
+tail -n +2 \
+> data.csv
 
