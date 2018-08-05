e584797fb888d0c3971b3117554492d91e78f5de
Jonas Sternisko <jonas.sternisko@adition.com>
2018-08-05 02:02:29 +0200

    Reword to "Registration"

--- globals.js
+++ globals.js
@@ -1,5 +1,5 @@
 var TOURNAMENT_NUMBER = "29th";
 
 function getTitle() {
-    return TOURNAMENT_NUMBER + " MischMasch HAT Player Information";
+    return TOURNAMENT_NUMBER + " MischMasch HAT Player Registration";
 }
