7657abfe7f1ff2ad6d4dc78374dead800353678d
Jonas Sternisko <jonas.sternisko@adition.com>
2018-08-06 08:21:46 +0200

    Add examples for credentials.php and payments/users.php

---
+++ credentials.example.php
@@ -0,0 +1,6 @@
+<?php
+// Rename to credentials.php and insert your database credentials.
+$username = "myUsername";
+$password = "myPassword";
+$database = "myDatabase";
+?>
---
+++ payments/users.example.php
@@ -0,0 +1,8 @@
+<?php
+// Customize users which are allowed to organize payments.
+// Rename to users.php and add users like so:
+$USERS = [
+    // "username1" => "password1",
+];
+
+?>
