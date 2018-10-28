098a84e9c248e8d7780a6e6a3888163fa1408317
Jonas Sternisko <jonas.sternisko@gmail.com>
2018-10-28 21:27:33 +0100

    Finish tutorial content

--- teams/teams-tutorial.js
+++ teams/teams-tutorial.js
@@ -7,38 +7,48 @@ function addPopovers() {
     popovers = [];
     popovers.push($("#team_assignment_table").popover({
         placement: "top", trigger: "manual", template: popoverTemplate,
-        title: "Step 1",
-        content: "team columns, d&amp;d"
+        title: "Team columns",
+        content: "The main part of the screen shows twelve team columns."
     }));
     popovers.push($("#player618").popover({
         placement: "right", trigger: "manual", template: popoverTemplate,
-        title: "Step 2 (player cards)",
-        content: ", gender-dependent background color, lets have a look into those breadcrumbs below ..."
+        title: "Player cards",
+        content: "Each player is represented by a draggable card. The card shows the player name and the data from the self-assessment. The player name has been shortened for privacy protection (European GDPR legislation applies). The background indicates the gender of the player. Some player names are highlighted. These are the so called locals. Each team has a local, which shows his team around the camping ground, the city and nearby bars between the tournament days. There is one local per team and thus they should not be moved to another team."
     }));
     popovers.push($("#player618 > div.row-fluid").popover({
         placement: "left", trigger: "manual", template: popoverTemplate,
-        title: "Step 2 (player cards - detail)",
+        title: "Player cards (detail)",
         content: "Each player assessed his own properties and skills during registration. Hover over the individual fields to see what they encode."
     }));
     popovers.push($("div#player752").popover({
         placement: "bottom", trigger: "manual", template: popoverTemplate,
-        title: "Step 3",
-        content: "column height = number of players per team"
+        title: "Players per team",
+        content: "As the cards are stacked up, the height of the columns encodes the total number of players in each team."
     }));
     popovers.push($("#summary-container-4").popover({
         placement: "top", trigger: "manual", template: popoverTemplate,
-        title: "Step 4",
-        content: "number of players per gender"
+        title: "Players per gender",
+        content: "The current number of male and female players per team is shown below."
+    }));
+    popovers.push($("#team-container-0-male").popover({
+        placement: "top", trigger: "manual", template: popoverTemplate,
+        title: "Unassigned players",
+        content: "On the bottom of the screen, there is space for players which have not yet been assigned to any team. As the tournament is in the past, this is empty."
+    }));
+    popovers.push($("#controlsRight").popover({
+        placement: "bottom", trigger: "manual", template: popoverTemplate,
+        title: "Credentials",
+        content: "Normally, only admins from the organization committee can change the player assignment. When not signed in, you would only be able to view the latest assignent. This has been used to stream the assignment process to the tournament participants. However, the credentials have been disabled for now to ease demonstration."
     }));
     popovers.push($("#controlsLeft").popover({
         placement: "bottom", trigger: "manual", template: popoverTemplate,
-        title: "Step 5",
-        content: "control panel: order by, show/hide information, ..."
+        title: "View controls",
+        content: "Here you can change the sort order of the players per team, which comes in helpful when balancing the strengths. Also the origin of each player can be shown. Finally, you can restart this tutorial at any later point and view the summary."
     }));
     popovers.push($("#showSummaryButton").popover({
         placement: "right", trigger: "manual", template: popoverTemplate,
-        title: "Step 6",
-        content: "Show summary now..."
+        title: "Summary visualization",
+        content: "Lets proceed with the summary window."
     }));
 }
 
@@ -69,4 +79,3 @@ function tutorialNextStep() {
     }
     popovers[currentPopover][0].scrollIntoView({ behavior: "smooth" });  // Not supported by all browsers yet.
 }
-
